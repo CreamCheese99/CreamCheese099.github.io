59f102f7e5e26e65775f7e189b96fe2379398a1b
Creamcheese <thanchira50045@gmail.com>
2025-03-22 17:07:19 +0700

    fix code

--- html/javascript/script.js
+++
@@ -1,8 +0,0 @@
-function updateClock() {
-    const now = new Date();
-    const timeString = now.toLocaleTimeString();
-    document.getElementById("clock").textContent = timeString;
-}
-
-setInterval(updateClock, 1000);
-updateClock();
